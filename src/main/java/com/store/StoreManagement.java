09b0a0c483ef2b749493388b0d8e60ff6e0b2137
gillespatrick <ngondogillespatrick@gmail.com>
2023-04-02 21:17:44 -0400

    Init projet

---
+++ src/main/java/com/store/StoreManagement.java
@@ -0,0 +1,14 @@
+
+
+package com.store;
+
+/**
+ *
+ * @author gilles
+ */
+public class StoreManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
